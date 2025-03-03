ed57b1e832b8664479c9d0980a645dbf70505c94
Mutiara <mutiarapandejlaki27@gmail.com>
2025-03-03 11:36:12 +0800

    struktur dasar login dan register page

--- src/js/view/pages/login.js
+++ src/js/view/pages/login.js
@@ -1,5 +1,16 @@
 const Login = {
-  async render() {}
+  async render() {
+    return `
+      <div class="loginSection">
+        <h2>Login</h2>
+        <form action="" class="login">
+          <input type="text" placeholder="Username">
+          <input type="password" placeholder="Password">
+        </form>
+        <button>Login</button>
+      </div>
+    `;
+  }
 };
 
 export default Login;
--- src/js/view/pages/register.js
+++ src/js/view/pages/register.js
@@ -1,5 +1,17 @@
 const Register = {
-  async render() {}
+  async render() {
+    return `
+      <div class="registerSection">
+        <h2>Register</h2>
+        <form action="" class="register">
+          <input type="text" placeholder="Username">
+          <input type="email" placeholder="Email">
+          <input type="password" placeholder="Password">
+        </form>
+        <button>Register</button>
+      </div>
+    `;
+  }
 };
 
 export default Register;
