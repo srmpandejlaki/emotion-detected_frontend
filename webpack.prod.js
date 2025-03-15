8fdf718b506c78b473e3a8495e010c8a85727b64
Mutiara <mutiarapandejlaki27@gmail.com>
2025-03-16 00:31:28 +0800

    akhirnya bisa build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,22 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-
+ 
 module.exports = merge(common, {
   mode: 'production',
   module: {
     rules: [
       {
-        test: /\.m?js$/,  // Tambahkan dukungan untuk .mjs (ES Module)
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            cacheDirectory: true, // Tambahkan cache agar build lebih cepat
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
-        },
+        ],
       },
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.mjs'], // Tambahkan ekstensi .mjs jika perlu
   },
 });
