9435446f85558a14f5586a09b27621733a11c518
Mutiara <mutiarapandejlaki27@gmail.com>
2025-04-13 12:16:04 +0800

    perbaiki warna dan posisi elemen

--- src/components/sideBar/navigationBar.jsx
+++ src/components/sideBar/navigationBar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="navBar">
-      <Link to="/dataCollection">Data Collection</Link>
-      <Link to="/preprocessing">Preprocessing</Link>
-      <Link to="/processing">Processing</Link>
-      <Link to="/validation">Validation</Link>
+      <Link to="/dataCollection" className="navItems">Data Collection</Link>
+      <Link to="/preprocessing" className="navItems">Preprocessing</Link>
+      <Link to="/processing" className="navItems">Processing</Link>
+      <Link to="/validation" className="navItems">Validation</Link>
     </div>
   )
 };
