67c423a184e927d48714ac459c497d6ef60de9ac
Mutiara <mutiarapandejlaki27@gmail.com>
2025-05-03 07:50:54 +0800

    hooks useInput

--- src/components/classifier/inputText.jsx
+++ src/components/classifier/inputText.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import useInput from "../../hooks/useInput";
 
-function InputText() {
+function InputText({ classify }) {
+  const [ text, onTextChange ] = useInput('');
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    classify({ text });
+  };
+
   return (
-    <div className="formClassifier">
-      <form action="">
-        <input type="text" placeholder="Input your text here" />
-      </form>
-      <button className="btn-process">Classify</button>
-    </div>
+    <form className="formClassifier" onSubmit={onSubmitHandler}>
+      <input type="text" value={text} onChange={onTextChange} placeholder="Input your text here" />
+      <button className="btn-process" type="submit">Classify</button>
+    </form>
   );
 };
 
