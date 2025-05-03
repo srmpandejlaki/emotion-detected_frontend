ba8fc914e5ef24cd84b284a70d8f34ea4877cc56
Mutiara <mutiarapandejlaki27@gmail.com>
2025-05-03 10:30:46 +0800

    tambah index.js

---
+++ src/utils/index.js
@@ -0,0 +1,36 @@
+const showFormattedDate = (date) => {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  return new Date(date).toLocaleDateString('id-ID', options);
+};
+
+const BASE_URL = 'http://127.0.0.1:8000/';
+
+function getAccessToken() {
+  return localStorage.getItem('accessToken');
+}
+
+function putAccessToken(accessToken) {
+  return localStorage.setItem('accessToken', accessToken);
+}
+
+async function fetchWithToken(url, options = {}) {
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+}
+
+export {
+  showFormattedDate,
+  getAccessToken,
+  putAccessToken,
+  fetchWithToken
+};
